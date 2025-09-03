b7edbf8a4a24dde549bd51f405aa87a18b6c47a0
Ankit Sen <ankitsen120983@gmail.com>
2025-09-03 14:00:17 +0530

    Update image_detection_model.py

--- image_detection_model.py
+++ image_detection_model.py
@@ -7,7 +7,7 @@ processor = AutoImageProcessor.from_pretrained("wambugu71/crop_leaf_diseases_vit
 model = AutoModelForImageClassification.from_pretrained("wambugu71/crop_leaf_diseases_vit")
 
 # Load an image (replace 'test_leaf.jpg' with your image path)
-image = Image.open(r"D:\image proccesing minor project\datasets\Train\Healthy Wheat\2KS7HG.jpg")
+image = Image.open(r"D:\download\bacterial_wheat_leaf.jpg")
 
 
 # Preprocess image
@@ -21,7 +21,3 @@ with torch.no_grad():
 logits = outputs.logits
 predicted_class_id = logits.argmax(-1).item()
 
-# Map to class name
-label = model.config.id2label[predicted_class_id]
-
-print(f"Predicted class: {label}")
